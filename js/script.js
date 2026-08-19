80bb2519b520e84183924b351605ad0113d4a1c5
QuratulainAzhar <bsef23m016@pucit.edu.pk>
2026-08-19 15:33:41 +0500

    Deployed Portfolio

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Mobile navigation
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+}
+
+
+// Contact form
+
+const contactForm = document.getElementById("contactForm");
+const formMessage = document.getElementById("formMessage");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", function() {
+
+       
+        const name = document.getElementById("name").value;
+
+        formMessage.textContent =
+            `Thanks, ${name}! Your message has been received.`;
+
+        contactForm.reset();
+
+    });
+
+}
